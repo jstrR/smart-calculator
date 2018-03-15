7c96f03994a027018c3e82e8912f5a51e4fc97a7
jstrR <srovk22@gmail.com>
2018-03-15 20:01:52 +0300

    100% dobe

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 class SmartCalculator {
-  constructor(initialValue) {
+  constructor(initialstack) {
     // your implementation
+    this.stack = Number(initialstack);
   }
 
   add(number) {
     // your implementation
+    this.stack +="+" + number;
+    return this;
   }
-  
+
   subtract(number) {
     // your implementation
+    this.stack += "-" + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.stack += "*" + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.stack += "/" + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.stack += "**" +number;
+    return this;
+  }
+
+  valueOf(){ //func 4test
+    return eval(this.stack);
   }
 }
 
